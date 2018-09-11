d1bb19974fb301d19a98b93bde347ae555443e8a
Yuval Hermelin <yuvalhermelin@Yuvals-MacBook-Pro.local>
2018-09-10 20:37:46 -0700

    test commit

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args){
         // System.out.println(bestHand.checkIfStraight(h));
         System.out.println(bestHand.identifyHand(h));
-        System.out.println("test");
+        System.out.println("hello");
 
         //NEW test commit
     }
